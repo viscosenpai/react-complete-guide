cffd1877db44ce9e7757d221fe6e5691c81f3ab0
KAMA Hiroki <kama@mediaxis.jp>
2020-09-07 23:31:14 +0900

    Sec 8, Lec-151

---
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import classes from "./ToolBar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+
+const toolBar = props => (
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
+
+export default toolBar;
