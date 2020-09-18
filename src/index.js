d2dfe478acc5fcf0e55d0e2ebbc5a74ac55845b8
KAMA Hiroki <kama@mediaxis.jp>
2020-09-18 15:41:19 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
