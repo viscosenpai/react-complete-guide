cacdbf56ee0ff3e1769dd30cef939498412008f1
KAMA Hiroki <kama@mediaxis.jp>
2020-09-22 15:48:04 +0900

    Sec 14, Lec-269

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import counterReducer from "./store/reducers/counter";
-import resultReducer from "./store/reducers/result";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-const rootReducer = combineReducers({
-  ctr: counterReducer,
-  res: resultReducer
-});
-
-const store = createStore(rootReducer);
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
