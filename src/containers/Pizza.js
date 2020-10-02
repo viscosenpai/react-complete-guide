5f6784a91273c54ee6669cd74b907bd472046701
KAMA Hiroki <kama@mediaxis.jp>
2020-10-03 00:04:29 +0900

    Sec 22, Lec-384

--- src/containers/Pizza.js
+++ src/containers/Pizza.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-import PizzaImage from "../components/PizzaImage/PizzaImage";
+import PizzaImage from '../components/PizzaImage/PizzaImage';
 
 class Pizza extends Component {
-  render() {
-    return (
-      <div>
-        <h1>The Pizza</h1>
-        <PizzaImage />
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div>
+                <h1>The Pizza</h1>
+                <PizzaImage />
+            </div>
+        );
+    }
 }
 
 export default Pizza;
