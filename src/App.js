4f3bd3dd90d854873ec0cc8eec6a84b087d51336
KAMA Hiroki <kama@mediaxis.jp>
2020-10-03 20:45:32 +0900

    Sec 24, Lec-404

--- src/App.js
+++ src/App.js
@@ -33,7 +33,14 @@ class App extends Component {
           in={this.state.showBlock} 
           timeout={1000}
           mountOnEnter
-          unmountOnExit>
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+        >
           {state => (
             <div
               style={{
@@ -46,11 +53,7 @@ class App extends Component {
               }}></div>
           )}
         </Transition>
-        <Transition in={this.state.modalIsOpen} mountOnEnter unmountOnExit timeout={300}>
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen ? <Backdrop show /> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
